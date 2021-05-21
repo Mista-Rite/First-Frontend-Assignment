7994b51cdd52e5f9af44035b06136ec645b6c507
Mista-Rite <ukaemmanueluche@gmail.com>
2021-05-21 20:23:31 +0100

    My Javascript assignment

---
+++ Assignment folder/Javascript-Assignment/script.js
@@ -0,0 +1,14 @@
+function convertFahrToCelsius(F){
+     let C = (F-32)*5/9
+     return C.toFixed(4);
+ }
+ let total=convertFahrToCelsius(0)
+ console.log(total);
+
+function checkYuGiOh(n){
+    let arry = [5, 6, 7, 8, 9, 4];
+    
+    
+};
+
+
